5b5754c532f4b5234350b7a789aa46c18bfddd17
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2025-08-10 15:14:01 +0700

    add code

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -4,43 +4,32 @@ use serde_json::json;
 // ---- CRUD for Questions ----
 
 pub async fn create_question(Json(question): Json<Question>) -> impl IntoResponse {
-    // todo!()
     Json(json!({
         "status": "ok 1",
-        // "received": question
     }))
 }
 
 pub async fn read_questions() -> impl IntoResponse {
-    // todo!()
     Json(json!({
         "status": "ok 2",
-        // "received": question
     }))
 }
 
 pub async fn delete_question(Json(question_uuid): Json<QuestionId>) {
-    // todo!()
-    // Json(json!({
-    //     "status": "ok 3",
-    //     // "received": question
-    // }))
-    // Json()
 }
 
 // ---- CRUD for Answers ----
+pub async fn create_answer(Json(question): Json<Answer>) -> impl IntoResponse {
+    Json(json!({
+        "status": "ok answer 1",
+    }))
+}
 
-// TODO: Create a POST route to /answer which accepts an `Answer` and returns `AnswerDetail` as JSON.
-//       The handler function should be called `create_answer`.
-//
-//       hint: this function should look very similar to the create_question function above
+pub async fn read_answers(Json(question_uuid): Json<QuestionId>) -> impl IntoResponse {
+    Json(json!({
+        "status": "ok answer 2",
+    }))
+}
 
-// TODO: Create a GET route to /answers which accepts an `QuestionId` and returns a vector of `AnswerDetail` as JSON.
-//       The handler function should be called `read_answers`.
-//
-//       hint: this function should look very similar to the read_questions function above
-
-// TODO: Create a DELETE route to /answer which accepts an `AnswerId` and does not return anything.
-//       The handler function should be called `delete_answer`.
-//
-//       hint: this function should look very similar to the delete_question function above
+pub async fn delete_answer(Json(answer_uuid): Json<AnswerId>) {
+}
--- src/main.rs
+++ src/main.rs
@@ -19,10 +19,10 @@ async fn main() {
         .route("/mystr", get(handler_mystr))
         .route("/question", post(create_question))
         .route("/questions", get(read_questions))
-        .route("/question", delete(delete_question));
-        // .route("/answer", post(create_answer))
-        // .route("/answers", get(read_answers))
-        // .route("/answer", delete(delete_answer));
+        .route("/question", delete(delete_question))
+        .route("/answer", post(create_answer))
+        .route("/answers", get(read_answers))
+        .route("/answer", delete(delete_answer));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
         .await
