4991c478031d3a3773cbf85e5ed4b28bfda711ed
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2025-08-10 14:08:39 +0700

    Add code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{
+    routing::{delete, get, post},
+    Router,
+    response::Html
+};
+
+// mod handlers;
+// mod models;
+
+// use handlers::*;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(handler))
+        .route("/mystr", get(handler_mystr));
+        // .route("/question", post(create_question))
+        // .route("/questions", get(read_questions))
+        // .route("/question", delete(delete_question))
+        // .route("/answer", post(create_answer))
+        // .route("/answers", get(read_answers))
+        // .route("/answer", delete(delete_answer));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
+        .await
+        .unwrap();
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
+}
+
+async fn handler_mystr() -> &'static str {
+    "Hello, World! Krub"
 }
