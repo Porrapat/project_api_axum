3ce102990ae69fdaf1ffa51dde62a80fa2937dab
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2025-08-10 14:14:33 +0700

    Add code

--- src/models.rs
+++ src/models.rs
@@ -2,16 +2,16 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 pub struct Question {
-    // TODO: add a public `title` field of type String
-    // TODO: add a public `description` field of type String
+    pub title: String,
+    pub description: String
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 pub struct QuestionDetail {
-    // TODO: add a public `question_uuid` field of type String
-    // TODO: add a public `title` field of type String
-    // TODO: add a public `description` field of type String
-    // TODO: add a public `created_at` field of type String
+    pub question_uuid: String,
+    pub title: String,
+    pub description: String,
+    pub created_at: String
 }
 
 // TODO: create a struct called `QuestionId`
