514b93592403ebb5c45203be2fb1d7be45f93f20
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2025-08-11 14:31:38 +0700

    Add code

--- src/main.rs
+++ src/main.rs
@@ -14,30 +14,31 @@ use handlers::*;
 
 use dotenvy::dotenv;
 
+use sqlx::postgres::PgPoolOptions;
+
 extern crate pretty_env_logger;
 #[macro_use] extern crate log;
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
-    info!("********* Question Records *********");
-    // TODO: Initialize dotenv
+    // info!("********* Question Records *********");
 
     dotenv().ok();
     let my_database_url = &std::env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
-    info!("********* {my_database_url} *********");
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(my_database_url)
+        .await
+        .expect("Failed to create Postgres connection pool!");
 
-    // Create a new PgPoolOptions instance with a maximum of 5 connections.
-    // Use dotenv to get the database url.
-    // Use the `unwrap` or `expect` method instead of handling errors. If an
-    // error occurs at this stage the server should be terminated.
-    // See examples on GitHub page: https://github.com/launchbadge/sqlx
-    // let pool = todo!();
+    let recs = sqlx::query!("SELECT * FROM questions")
+        .fetch_all(&pool)
+        .await
+        .unwrap();
 
-    // Using slqx, execute a SQL query that selects all questions from the questions table.
-    // Use the `unwrap` or `expect` method to handle errors. This is just some test code to
-    // make sure we can connect to the database.
-    // let recs = todo!();
+    info!("********* Question Records *********");
+    info!("{:?}", recs);
 
     let app = Router::new()
         .route("/", get(handler))
