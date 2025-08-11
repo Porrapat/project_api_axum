b84708241ed19b0e18a8caad776e9122dfac4104
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2025-08-11 12:29:00 +0700

    add code

---
+++ manual_sql_axum.sql
@@ -0,0 +1,3 @@
+INSERT INTO "questions" ("question_uuid", "title", "description", "created_at") VALUES ('82152280-b5ce-44b1-aac4-7c4a2fae8bfe', 'Question 1', 'This is the description for question 1', NOW());
+INSERT INTO "questions" ("question_uuid", "title", "description", "created_at") VALUES ('9fc23a11-9411-4bf9-beda-6d52d688c409', 'Question 2', 'This is the description for question 2', NOW());
+INSERT INTO "questions" ("question_uuid", "title", "description", "created_at") VALUES ('bec90455-57b8-4acd-9a93-d295d43289fe', 'Question 3', 'This is the description for question 3', NOW());
