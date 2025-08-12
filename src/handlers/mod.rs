d6142898d620b75ad343457fbeeb2181a85708c1
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2025-08-12 12:31:07 +0700

    Add all code first!

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,109 +1,73 @@
-use crate::models::*;
-use axum::{extract::{Path}, response::IntoResponse, Json};
-use serde_json::json;
+use axum::{
+    extract::State as AxumState, http::StatusCode, response::IntoResponse, Json as JsonAxum,
+};
+
+use crate::{models::*, AppState};
 
 mod handlers_inner;
+
+impl IntoResponse for handlers_inner::HandlerError {
+    fn into_response(self) -> axum::response::Response {
+        match self {
+            handlers_inner::HandlerError::BadRequest(msg) => {
+                (StatusCode::BAD_REQUEST, msg).into_response()
+            }
+            handlers_inner::HandlerError::InternalError(msg) => {
+                (StatusCode::INTERNAL_SERVER_ERROR, msg).into_response()
+            }
+        }
+    }
+}
+
 // ---- CRUD for Questions ----
 
-pub async fn create_question(Json(question): Json<Question>) -> impl IntoResponse {
-    info!("********* create_question *********");
-
-    // let pretty = serde_json::to_string_pretty(&QuestionDetail {
-    //     question_uuid: "abced".to_owned(),
-    //     title: "My title".to_owned(),
-    //     description: "My Description".to_owned(),
-    //     created_at: "2025-08-11 12:00:00".to_owned()
-    // }).unwrap();
-
-    // (
-    //     axum::http::StatusCode::OK,
-    //     [(axum::http::header::CONTENT_TYPE, "application/json")],
-    //     pretty
-    // )
-
-    Json(json!(QuestionDetail {
-        question_uuid: "abced".to_owned(),
-        title: "My title".to_owned(),
-        description: "My Description".to_owned(),
-        created_at: "2025-08-11 12:00:00".to_owned()
-    }))
+pub async fn create_question(
+    AxumState(AppState { questions_dao, .. }): AxumState<AppState>,
+    JsonAxum(question): JsonAxum<Question>,
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    handlers_inner::create_question(question, questions_dao.as_ref())
+        .await
+        .map(JsonAxum)
 }
 
-pub async fn read_questions() -> impl IntoResponse {
-    info!("********* read_questions *********");
-    Json(json!(vec![
-        QuestionDetail {
-            question_uuid: "abced".to_owned(),
-            title: "My title 1".to_owned(),
-            description: "My Description".to_owned(),
-            created_at: "2025-08-11 12:00:00".to_owned()
-        },
-        QuestionDetail {
-            question_uuid: "abced".to_owned(),
-            title: "My title 2".to_owned(),
-            description: "My Description".to_owned(),
-            created_at: "2025-08-11 12:00:00".to_owned()
-        },
-        QuestionDetail {
-            question_uuid: "abced".to_owned(),
-            title: "My title 3".to_owned(),
-            description: "My Description".to_owned(),
-            created_at: "2025-08-11 12:00:00".to_owned()
-        }
-    ]))
+pub async fn read_questions(
+    AxumState(AppState { questions_dao, .. }): AxumState<AppState>,
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    handlers_inner::read_questions(questions_dao.as_ref())
+        .await
+        .map(JsonAxum)
 }
 
-pub async fn delete_question(Json(question_uuid): Json<QuestionId>) {
-    info!("********* delete_question *********");
+pub async fn delete_question(
+    AxumState(AppState { questions_dao, .. }): AxumState<AppState>,
+    JsonAxum(question_uuid): JsonAxum<QuestionId>,
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    handlers_inner::delete_question(question_uuid, questions_dao.as_ref()).await
 }
 
 // ---- CRUD for Answers ----
-pub async fn create_answer(Json(question): Json<Answer>) -> impl IntoResponse {
-    info!("********* create_answer *********");
-    Json(json!(AnswerDetail {
-        answer_uuid: "aaaa".to_owned(),
-        question_uuid: "abced".to_owned(),
-        content: "This is content".to_owned(),
-        created_at: "2025-08-11 12:00:00".to_owned()
-    }))
+
+pub async fn create_answer(
+    AxumState(AppState { answers_dao, .. }): AxumState<AppState>,
+    JsonAxum(answer): JsonAxum<Answer>,
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    handlers_inner::create_answer(answer, answers_dao.as_ref())
+        .await
+        .map(JsonAxum)
 }
 
-pub async fn read_answers(Path(question_uuid): Path<String>) -> impl IntoResponse {
-    info!("********* read_answers *********");
-    Json(json!(vec![
-        AnswerDetail {
-            answer_uuid: "aaaa".to_owned(),
-            question_uuid: question_uuid.to_owned(),
-            content: "This is content".to_owned(),
-            created_at: "2025-08-11 12:00:00".to_owned()
-        },
-        AnswerDetail {
-            answer_uuid: "aaaa".to_owned(),
-            question_uuid: question_uuid.to_owned(),
-            content: "This is content".to_owned(),
-            created_at: "2025-08-11 12:00:00".to_owned()
-        },
-        AnswerDetail {
-            answer_uuid: "aaaa".to_owned(),
-            question_uuid: question_uuid.to_owned(),
-            content: "This is content".to_owned(),
-            created_at: "2025-08-11 12:00:00".to_owned()
-        },
-        AnswerDetail {
-            answer_uuid: "aaaa".to_owned(),
-            question_uuid: question_uuid.to_owned(),
-            content: "This is content".to_owned(),
-            created_at: "2025-08-11 12:00:00".to_owned()
-        },
-        AnswerDetail {
-            answer_uuid: "aaaa".to_owned(),
-            question_uuid: question_uuid.to_owned(),
-            content: "This is content".to_owned(),
-            created_at: "2025-08-11 12:00:00".to_owned()
-        }
-    ]))
+pub async fn read_answers(
+    AxumState(AppState { answers_dao, .. }): AxumState<AppState>,
+    JsonAxum(question_uuid): JsonAxum<QuestionId>,
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    handlers_inner::read_answers(question_uuid, answers_dao.as_ref())
+        .await
+        .map(JsonAxum)
 }
 
-pub async fn delete_answer(Json(answer_uuid): Json<AnswerId>) {
-    info!("********* delete_answer *********");
+pub async fn delete_answer(
+    AxumState(AppState { answers_dao, .. }): AxumState<AppState>,
+    JsonAxum(answer_uuid): JsonAxum<AnswerId>,
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    handlers_inner::delete_answer(answer_uuid, answers_dao.as_ref()).await
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
-#![allow(unused)]
-use std::net::SocketAddr;
+#[macro_use]
+extern crate log;
+
+extern crate pretty_env_logger;
+
+use std::sync::Arc;
 
 use axum::{
     routing::{delete, get, post},
     Router,
-    response::Html
 };
+use dotenvy::dotenv;
+
+use persistance::{
+    answers_dao::{AnswersDao, AnswersDaoImpl},
+    questions_dao::{QuestionsDao, QuestionsDaoImpl},
+};
+use sqlx::postgres::PgPoolOptions;
 
 mod handlers;
 mod models;
@@ -13,83 +23,43 @@ mod persistance;
 
 use handlers::*;
 
-use dotenvy::dotenv;
-
-use sqlx::postgres::PgPoolOptions;
-
-extern crate pretty_env_logger;
-#[macro_use] extern crate log;
-
-use persistance::{
-    answers_dao::{AnswersDao, AnswersDaoImpl},
-    questions_dao::{QuestionsDao, QuestionsDaoImpl},
-};
-
-use models::Question;
+#[derive(Clone)]
+pub struct AppState {
+    pub questions_dao: Arc<dyn QuestionsDao + Send + Sync>,
+    pub answers_dao: Arc<dyn AnswersDao + Send + Sync>,
+}
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
-    // info!("********* Question Records *********");
-
     dotenv().ok();
-    let my_database_url = &std::env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
+
     let pool = PgPoolOptions::new()
         .max_connections(5)
-        .connect(my_database_url)
+        .connect(&std::env::var("DATABASE_URL").expect("DATABASE_URL must be set."))
         .await
         .expect("Failed to create Postgres connection pool!");
 
-    // let doa = QuestionsDaoImpl::new(pool);
+    let questions_dao = Arc::new(QuestionsDaoImpl::new(pool.clone()));
+    let answers_dao = Arc::new(AnswersDaoImpl::new(pool));
 
-    // let results = doa.get_questions().await.expect("Error Here!!!!");
-    // println!("{:?}", results);
-
-    // let a_doa = AnswersDaoImpl::new(pool);
-    // println!("Before Delete");
-    // a_doa.delete_answer("55790455-57b8-4acd-9a93-d295d4328e".to_owned()).await.expect("Error Here!!!!");
-    // println!("Delete OK");
-    // let a_results = a_doa.get_answers("82152280-b5ce-44b1-aac4-7c4a2fae8bfe".to_owned()).await.expect("Error Here!!!!");
-    // println!("{:?}", a_results);
-    // let result = doa
-    //     .create_question(Question {
-    //         title: "test xxx title".to_owned(),
-    //         description: "test xxx description".to_owned(),
-    //     })
-    //     .await
-    //     .map_err(|e| format!("{:?}", e)).expect("Error HERE!!!!!");
-
-    // let recs = sqlx::query!("SELECT * FROM questions")
-    //     .fetch_all(&pool)
-    //     .await
-    //     .unwrap();
-
-    // info!("********* Question Records *********");
-    // info!("{:?}", recs);
+    let app_state = AppState {
+        questions_dao,
+        answers_dao,
+    };
 
     let app = Router::new()
-        .route("/", get(handler))
-        .route("/mystr", get(handler_mystr))
         .route("/question", post(create_question))
         .route("/questions", get(read_questions))
         .route("/question", delete(delete_question))
         .route("/answer", post(create_answer))
-        .route("/answers/{question_uuid}", get(read_answers))
-        .route("/answer", delete(delete_answer));
+        .route("/answers", get(read_answers))
+        .route("/answer", delete(delete_answer))
+        .with_state(app_state);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
         .await
         .unwrap();
 
-    println!("listening on {}", listener.local_addr().unwrap());
-
     axum::serve(listener, app).await.unwrap();
-}
-
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
-}
-
-async fn handler_mystr() -> &'static str {
-    "Hello, World! Krub"
 }
